4ab5117914a5c92962bafcce237c5e0faec4ec94
Екатерина <Burpumba@mail.ru>
2017-05-04 21:29:05 +0300

    Добавлен начальный интерфейс #2

---
+++ Mafia/Mafia/Sound.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace Mafia
+{
+ /// <summary>
+ /// Проигрование звука
+ /// </summary>
+     static class Sound
+    {
+        /// <summary>
+        /// Экземпляр класса поигрования звука 
+        /// </summary>
+        static SoundPlayer _soundfail = new SoundPlayer(Properties.Resources.twenty);
+        static bool sound_enabled = true;
+        /// <summary>
+        /// Включение звука
+        /// </summary>
+        public static void sound_on()
+        {
+            sound_enabled = true;
+        }
+        /// <summary>
+        /// Выключение звука 
+        /// </summary>
+        public static void sound_off()
+        {
+            sound_enabled = false;
+        }
+        /// <summary>
+        /// Воспроизведение звука
+        /// </summary>
+        public static void Play_fail()
+        {
+            if(sound_enabled)
+            _soundfail.Play();
+        }
+    }
+}
